182248afc7ab9846e595e5ce48d59863bf592432
NativeCode <tm.nicolasmachuca@gmail.com>
2026-04-23 10:57:59 -0400

    sign

--- src/app/sign-in/[[...sign-in]]/page.jsx
+++ src/app/sign-in/[[...sign-in]]/page.jsx
@@ -103,7 +103,7 @@ export default function Page() {
               <img
                 src="/logo.png"
                 alt="AgendaClinica"
-                className="h-10 w-14 object-contain object-left"
+                className="h-14 w-20 object-contain object-left"
               />
               <div>
                 <p className={michroma.className + " text-[13px] text-slate-900"}>AgendaClinica</p>
